311ed86aa8b71045c58984bce569d707d40a8049
unknown <zakilouhidi@gmail.com>
2023-02-05 19:42:02 +0100

    Exemple d'implémentation d'un bouton Partage

--- ShareImage.js
+++ ShareImage.js
@@ -1,18 +1,48 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Share from 'react-native-share';
 
-export const ShareImage = ({ imageUrl }) => {
-  const shareImage = () => {
-    Linking.openURL(`instagram://share?image=${encodeURIComponent(imageUrl)}`);
+export const ShareImage = () => {
+  const [imageUrl, setImageUrl] = useState(
+    'https://myblazon.com/api/images/abfb5b4c?'
+  );
+  const onShare = async () => {
+    try {
+      const result = await Share.open({
+        url: imageUrl
+      });
+    } catch (error) {
+      alert(error.message);
+    }
   };
-
   return (
-    <View>
-      <Image source={{ uri: imageUrl }} />
-      <TouchableOpacity onPress={shareImage}>
-        <Text>Partager sur Instagram</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onShare} style={styles.button}>
+        <Text style={styles.buttonText}>Partager</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 40
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 20,
+    borderRadius: 5,
+    marginBottom: 80
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20
+  }
+});
 
